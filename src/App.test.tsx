a9df4612f09f8563b4d5180ecde05ae3a13bc5cd
Diovane Gualberto <diov@outlook.com>
2023-01-12 22:10:19 -0300

    Add msw and integration test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+import {render, fireEvent, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './App'
+import env from './setupEnv';
+import location from 'location'
 
-test('renders learn react link', () => {
+const server = setupServer(
+  rest.post(env.baseURL + '/crawl', (req, res, ctx) => {
+    return res(ctx.json({id: '#id'}))
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+test('add new query to the list', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const input = screen.getByPlaceholderText(location.searchPlaceholder);
+
+  fireEvent.change(input, { target: { value: 'test'}});
+  fireEvent.click(screen.getByText(location.search));
+
+  await screen.findByText('#id');
+
+  expect(screen.getByText('#id')).toBeInTheDocument();
 });
